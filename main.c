1b6d3cb7cda83f1b80dd9dafa1b06082c78dc068
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-11-12 18:43:14 -0300

    :sparkles: Versão string local implementada.

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 #define LARGEST_WORD 46
 
 void formataEntrada(char *arquivo){
-  // for (int i = 0; i < strlen(string); i++) string[i] = tolower(string[i]);
   FILE *arq = fopen(arquivo, "r");
   char aux;
   char *string;
@@ -23,16 +22,32 @@ void formataEntrada(char *arquivo){
     printf("%s\n", string);
     free(string);
   }while(aux != EOF);
+}
 
+void formataEntradaString(char *string){
+  char aux;
+  char *stringAux = calloc(LARGEST_WORD, sizeof(char));
+  int i = 0, j;
 
+  for(int j = 0; j <= strlen(string); j++){
+    if(!isspace(string[j]) && !ispunct(string[j]) && string[j] != 0){
+      stringAux[i++] = tolower(string[j]);
+    }else{
+      printf("%s\n", stringAux);
+      free(stringAux);
+      stringAux = calloc(LARGEST_WORD, sizeof(char));
+      i = 0;
+    }
+  }
+  free(stringAux);
 }
 
 int main(int argc, char** argv){
-  // char string[] = "OLA ESTE E UM TESTE SE NOSSA FUNCAO ESTA FUNCIONANDO CORRETAMENTE";
-
+  char string[] = "OLA ESTE E UM TESTE SE NOSSA FUNCAO ESTA FUNCIONANDO CORRETAMENTE";
+  formataEntradaString(string);
 
   // printf("INICIO: %s\n", string);  
-  formataEntrada("testes/casmurro1.txt");
+  // formataEntrada("testes/casmurro1.txt");
   // printf("FIM: %s\n", string);
 
   // for(int i = 0; i < 256; i++){
