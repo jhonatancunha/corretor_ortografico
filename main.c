75ea4a0e4935cfd885f8a36ff2d31f106cf80143
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2020-11-12 18:27:30 -0300

    :sparkles: Implementada função que obtém palavras do arquivo teste, retirando pontuações e aplicando tolower.

--- main.c
+++ main.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <locale.h>
 #include <ctype.h>
 
-void formataEntrada(char *string){
-  for (int i = 0; i < strlen(string); i++){
-    string[i] = tolower(string[i]);
-    switch ((int)string[i]){
-      case 224 ... 229:
-        string[i] = 'a';
-        break;
-      case 232 ... 235:
-        string[i] = 'e';
-        break;
-      case 236 ... 239:
-        string[i] = 'i';
-        break;
-      case 242 ... 246:
-        string[i] = 'o';
-        break;
-      case 249 ... 252:
-        string[i] = 'u';
-        break;
-    }  
-  }
+#define LARGEST_WORD 46
+
+void formataEntrada(char *arquivo){
+  // for (int i = 0; i < strlen(string); i++) string[i] = tolower(string[i]);
+  FILE *arq = fopen(arquivo, "r");
+  char aux;
+  char *string;
+  int i;
+
+  do{
+    string = calloc(LARGEST_WORD, sizeof(char));
+    i = 0;
+    while(1){
+      aux = fgetc(arq);
+      if(isspace(aux) || ispunct(aux) || aux == EOF) break;
+      string[i++] = tolower(aux);
+    };
+    printf("%s\n", string);
+    free(string);
+  }while(aux != EOF);
+
+
 }
 
 int main(int argc, char** argv){
-  setlocale(LC_ALL, "");
-  char string[] = "OLÁ ESTE É UM TESTE SE NOSSA FUNÇÃO ESTÁ FUNCIONANDO CORRETAMENTE";
+  // char string[] = "OLA ESTE E UM TESTE SE NOSSA FUNCAO ESTA FUNCIONANDO CORRETAMENTE";
 
 
-  printf("INICIO: %s\n", string);  
-  formataEntrada(string);
-  printf("FIM: %s\n", string);
+  // printf("INICIO: %s\n", string);  
+  formataEntrada("testes/casmurro1.txt");
+  // printf("FIM: %s\n", string);
 
+  // for(int i = 0; i < 256; i++){
+  //   printf("%c - %d\n", i, i);
+  // }
 
   return 0;
 }
