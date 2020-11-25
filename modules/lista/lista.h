fd0bcaa0318b92d87aef629736921e7131911278
Jesse Pires <jessepires2010@gmail.com>
2020-11-25 16:05:51 -0300

    :bulb: Adicionando comentários para os protótipos das funções

--- modules/lista/lista.h
+++ modules/lista/lista.h
@@ -6,10 +6,55 @@ typedef struct LISTA {
   int quantidade_atual;
 } LISTA;
 
+/* FUNÇÃO QUE CRIA UMA LISTA
+*
+*============RETORNO============
+*return: endereço da lista criada
+*/
 LISTA* LISTA_Criar ();
+
+/* FUNÇÃO QUE INSERE UMA STRING NA LISTA
+*
+*============ARGUMENTOS============
+* l = endereço da lista
+* palavra = string que será inserida
+*============RETORNO============
+*return: void
+*/
 void LISTA_Inserir (LISTA* l, char* palavra);
+
+/* FUNÇÃO QUE VERIFICA SE A LISTA ESTÁ VAZIA OU NÃO
+*
+*============ARGUMENTOS============
+* l = endereço da lista
+*============RETORNO============
+*return: 1 caso esteja vazia e 0 caso contrário
+*/
 int LISTA_isVazia (LISTA* l);
+
+/* FUNÇÃO QUE RETORNA A MAIOR PALAVRA DA LISTA
+*
+*============ARGUMENTOS============
+* l = endereço da lista
+*============RETORNO============
+*return: string com a maior palavra
+*/
 char* LISTA_MaiorPalavra (LISTA* l);
+
+/* FUNÇÃO QUE IMPRIME AS PALAVRAS DA LISTA
+*
+*============ARGUMENTOS============
+* l = endereço da lista
+*============RETORNO============
+*return: void
+*/
 void LISTA_Imprimir (LISTA* l);
+
+/* FUNÇÃO QUE DESTRÓI TOTALMENTE A LISTA
+*
+*============ARGUMENTOS============
+* l = ponteiro de ponteiro de LISTA
+*============RETORNO============
+*return: void
+*/
 void LISTA_Destruir (LISTA** l);
-void ordenaLista(LISTA *l);
