c4075cd67a0c11271a9a8e72ff2529b05ab95fd5
Jesse Pires <jessepires2010@gmail.com>
2020-11-25 15:53:57 -0300

    :bulb: Adicionando comentários

--- modules/dicionario/dict.h
+++ modules/dicionario/dict.h
@@ -3,4 +3,12 @@
 
 #define LARGEST_WORD 47
 
+/* ESSA FUNÇÃO ACESSA UM ARQUIVO COM AS PALAVRAS DO DICIONÁRIO E RETORNA UMA
+TRIE POPULADA COM TAIS PALAVRAS
+*
+*============ARGUMENTOS============
+*dicionario = nome do arquivo que contem as palavras
+*============RETORNO============
+*return: ASCIITrie populada com as palavras corretas
+*/
 ASCIITrie* criarDicionario(char *dicionario);
